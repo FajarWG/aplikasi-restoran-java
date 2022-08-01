7a50479a217218d1bc4e591fce8f0520fae971de
MujahidFR <mujahidfathulrabbani442@gmail.com>
2022-08-01 17:47:44 +0700

    MVC-1

---
+++ src/aplikasirestoran/kelompok6/pbo/controller/PelangganController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasirestoran.kelompok6.pbo.controller;
+
+/**
+ *
+ * @author Asus N751J
+ */
+public class PelangganController {
+    
+}
